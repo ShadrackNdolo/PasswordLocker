c3cb229ea2e9d3d15845c13fcdd8ff113b79d42e
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-23 12:25:30 +0300

    Add a test for saving user info

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,25 +1,22 @@
 import unittest
-  import pyperclip
- -#from user_credentials import User, Credentials 
- +from user_credentials import User
+ import pyperclip
+  from user_credentials import User
   
-  class TestUserCredentials(unittest.TestCase):
+ -class TestUserCredentials(unittest.TestCase):
+ +class TestUser(unittest.TestCase):
   	'''
- @@ -13,7 +13,15 @@ def setUp(self):
-  		'''
-  		Function to create a user account before each test
-  		'''
- -		self.new_user = User('Mary','pswd100')
- +		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
- +
- +	def test__init__(self):
- +		'''
- +		Test to if check the initialization/creation of instances is properly done
- +		'''
- +		self.assertEqual(self.new_user.first_name,'Mary')
- +		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
- +		self.assertEqual(self.new_user.password,'pswd100')
+  	Test class that defines test cases for the contact class behaviours.
   
+ @@ -23,5 +23,12 @@ def test__init__(self):
+  		self.assertEqual(self.new_user.last_name,'N\'dol\'o')
+  		self.assertEqual(self.new_user.password,'pswd100')
+  
+ +	def test_save_user(self):
+ +		'''
+ +		Test to check if the new users info is saved into the users list
+ +		'''
+ +		self.new_user.save_user()
+ +		self.assertEqual(len(User.users_list),1)
+ +
   if __name__ == '__main__':
- -	unittest.main() 
- +	unittest.main(verbosity=2)
+  	unittest.main(verbosity=2) 
