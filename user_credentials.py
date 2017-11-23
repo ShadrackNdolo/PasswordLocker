bdea0732328905141fab603d1c2d9540fa3abdbf
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-23 12:34:52 +0300

    Add a function for saving the users entered info.

--- user_credentials.py
+++ user_credentials.py
@@ -1,16 +1,25 @@
- import pyperclip
+  import pyperclip
   
- +users_list = []
+ +# Global Variables
+ +global users_list 
   class User:
   	'''
   	Class to create user accounts and save their information
-	'''
- 	def __init__(self,first_name,second_name,password):
- 		'''
-		Method to define the properties for each user object will hold.
-		'''
-
-		# instance variables
-		self.first_name = first_name
-		self.last_name = last_name
-		self.password = password
+  	'''
+ +	# Class Variables
+ +	users_list =[]
+  	def __init__(self,first_name,last_name,password):
+  		'''
+  		Method to define the properties for each user object will hold.
+ @@ -13,4 +16,12 @@ def __init__(self,first_name,last_name,password):
+  		# instance variables
+  		self.first_name = first_name
+  		self.last_name = last_name
+ +		self.password = password
+ +
+ +	def save_user(self):
+ +		'''
+ +		Function to save a newly created user instance
+ +		'''
+ +		# global users_list
+ +		User.users_list.append(self)
