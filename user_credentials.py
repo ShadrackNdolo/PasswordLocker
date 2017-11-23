89915d417cbd2b409c56963523c3ee9d200d8abe
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-23 13:30:16 +0300

    Create a test to verify class method find_by_site_name works properly

--- user_credentials.py
+++ user_credentials.py
@@ -46,3 +46,14 @@ class Credential:
 		'''
         # global users_list
         Credential.credentials_list.append(self)
+
+        return cls.credentials_list
+g9i		
+	@classmethod
+	def find_by_site_name(cls, site_name):
+		'''
+ 	Method that takes in a site_name and returns a credential that matches that site_name.
+		'''
+		for credential in cls.credentials_list:
+			if credential.site_name == site_name:
+				return credential
