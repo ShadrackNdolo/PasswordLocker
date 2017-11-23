620e800ceff56b555f69bc517beef0538c5f91d9
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-23 12:54:00 +0300

    Create a test case for the class Credential.

--- user_credentials.py
+++ user_credentials.py
@@ -1,13 +1,13 @@
   import pyperclip
   
- +# Global Variables
- +global users_list 
+ # Global Variables
+ global users_list 
   class User:
   	'''
   	Class to create user accounts and save their information
   	'''
- +	# Class Variables
- +	users_list =[]
+      	# Class Variables
+	users_list =[]
   	def __init__(self,first_name,last_name,password):
   		'''
   		Method to define the properties for each user object will hold.
@@ -15,25 +15,25 @@
   		# instance variables
   		self.first_name = first_name
   		self.last_name = last_name
- +		self.password = password
- +
- +	def save_user(self):
- +		'''
- +		Function to save a newly created user instance
- +		'''
- +		# global users_list
- +		User.users_list.append(self)
+		self.password = password
 
- +class Credential:
- +	'''
- +	Class to create  account credentials, generate passwords and save their information
- +	'''
- +	def __init__(self,site_name,account_name,password):
- +		'''
- +		Method to define the properties for each user object will hold.
- +		'''
- +
- +		# instance variables
- +		self.site_name = site_name
- +		self.account_name = account_name
- +		self.password = password
+	def save_user(self):
+		'''
+ 		Function to save a newly created user instance
+		'''
+		# global users_list
+		User.users_list.append(self)
+
+class Credential:
+	'''
+	Class to create  account credentials, generate passwords and save their information
+	'''
+	def __init__(self,site_name,account_name,password):
+ 		'''
+	Method to define the properties for each user object will hold.
+ 		'''
+
+		# instance variables
+		self.site_name = site_name
+		self.account_name = account_name
+		self.password = password
--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,22 +1,34 @@
 import unittest
  import pyperclip
-  from user_credentials import User
+  from user_credentials import User, Credential
   
- -class TestUserCredentials(unittest.TestCase):
- +class TestUser(unittest.TestCase):
+class TestUser(unittest.TestCase):
   	'''
-  	Test class that defines test cases for the contact class behaviours.
+  	Test class that defines test cases for the user class behaviours.
   
  @@ -23,5 +23,12 @@ def test__init__(self):
   		self.assertEqual(self.new_user.last_name,'N\'dol\'o')
   		self.assertEqual(self.new_user.password,'pswd100')
   
- +	def test_save_user(self):
- +		'''
- +		Test to check if the new users info is saved into the users list
- +		'''
- +		self.new_user.save_user()
- +		self.assertEqual(len(User.users_list),1)
- +
+	def test_save_user(self):
+		'''
+		Test to check if the new users info is saved into the users list
+		'''
+		self.new_user.save_user()
+		self.assertEqual(len(User.users_list),1)
+
+class TestCredentials(unittest.TestCase):
+	'''
+	Test class that defines test cases for the credentials class behaviours.
+
+	Args:
+	    unittest.TestCase: helps in creating test cases
+	'''
+ 	def setUp(self):
+		'''
+		Function to create an account's credentials before each test
+		'''
+ 		self.new_account = Credential('Soundcloud','Shaw','pswd100')
+
   if __name__ == '__main__':
   	unittest.main(verbosity=2) 
