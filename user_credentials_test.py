a13946c9371697eb9bc13a6b29d8337d2e34c552
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-23 12:12:09 +0300

    Create a test to check for proper initialization of user objects. 1. Import the class User from user_credentials.py 2. Add a property last_name to the user __init__.

--- user_credentials_test.py
+++ user_credentials_test.py
@@ -1,19 +1,25 @@
- import  unittest
- import pyperclip
- #from user_credentials import User, Credentials 
- 
- class TestUserCredentials(unittest.TestCase):
- 	'''
- 	Test class that defines test cases for the contact class behaviours.
- 
- 	Args:
- 	    unittest.TestCase: helps in creating test cases
- 	'''
- 	def setUp(self):
- 		'''
- 		Function to create a user account before each test
- 		'''
- 		self.new_user = User('Shaw','pswd100')
- 
- if __name__ == '__main__':
- 	unittest.main() 
+import unittest
+  import pyperclip
+ -#from user_credentials import User, Credentials 
+ +from user_credentials import User
+  
+  class TestUserCredentials(unittest.TestCase):
+  	'''
+ @@ -13,7 +13,15 @@ def setUp(self):
+  		'''
+  		Function to create a user account before each test
+  		'''
+ -		self.new_user = User('Mary','pswd100')
+ +		self.new_user = User('Mary','Ng\'ang\'a','pswd100')
+ +
+ +	def test__init__(self):
+ +		'''
+ +		Test to if check the initialization/creation of instances is properly done
+ +		'''
+ +		self.assertEqual(self.new_user.first_name,'Mary')
+ +		self.assertEqual(self.new_user.last_name,'Ng\'ang\'a')
+ +		self.assertEqual(self.new_user.password,'pswd100')
+  
+  if __name__ == '__main__':
+ -	unittest.main() 
+ +	unittest.main(verbosity=2)
